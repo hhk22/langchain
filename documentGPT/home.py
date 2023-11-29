c912249cebf5f1469b1056ea25227eafba05941a
Hyeonghwan <khhh9401@gmail.com>
2023-11-29 23:01:43 +0900

    markdown update

--- documentGPT/home.py
+++ documentGPT/home.py
@@ -13,8 +13,8 @@ Welcome to my FullstackGPT Portfolio!
             
 Here are the apps I made:
             
-- [ ] [DocumentGPT](/DocumentGPT)
-- [ ] [PrivateGPT](/PrivateGPT)
+- [O] [DocumentGPT](/DocumentGPT)
+- [O] [PrivateGPT](/PrivateGPT)
 - [ ] [QuizGPT](/QuizGPT)
 - [ ] [SiteGPT](/SiteGPT)
 - [ ] [MeetingGPT](/MeetingGPT)
