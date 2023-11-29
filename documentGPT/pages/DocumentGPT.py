3d65b3758adbb8cd877c3b29e1f84ef7df812572
Hyeonghwan <khhh9401@pioncorp.com>
2023-11-29 17:36:33 +0900

    documentGPT intermediate result

--- documentGPT/pages/DocumentGPT.py
+++ documentGPT/pages/DocumentGPT.py
@@ -21,13 +21,31 @@ st.title("DocumentGPT")
 st.markdown(
     """
         Welcome!
-                    
+
         Use this chatbot to ask questions to an AI about your files!
 
         Upload your files on the sidebar.
 """
 )
 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+
+            Context: {context}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+llm = ChatOpenAI(temperature=0.1)
+
+
+@st.cache_data(show_spinner="Embedding files...")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
@@ -39,11 +57,8 @@ def embed_file(file):
         chunk_size=600,
         chunk_overlap=100,
     )
-    
-    state_of_the_union = None
-    with open('./rag/test.txt') as f:
-        state_of_the_union = f.read()
-    texts = state_of_the_union.split("\n\n")
+
+    texts = file_content.decode().split("\n\n")
     docs = splitter.create_documents(texts)
 
     embeddings = OpenAIEmbeddings()
@@ -52,8 +67,41 @@ def embed_file(file):
     retriever = vectorstore.as_retriever()
     return retriever
 
-file = st.file_uploader("Upload a file", type=["pdf", "txt", "docx"])
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
+
+
+def format_docs(docs):
+    return "\n\n".join([doc.page_content for doc in docs])
+
+
+file = None
+with st.sidebar:
+    file = st.file_uploader("Upload a file", type=["pdf", "txt", "docx"])
+
 if file:
     retriver = embed_file(file)
-    s = retriver.invoke("where do you live?")
-    st.write(s)
+    send_message("I'm Ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    if message:
+        send_message(message, "human")
+        chain = (
+            {
+                "context": retriver | RunnableLambda(format_docs),
+                "question": RunnablePassthrough()
+            } | prompt | llm
+        )
+        response = chain.invoke(message)
+        send_message(response.content, "ai")
+else:
+    st.session_state["messages"] = []
